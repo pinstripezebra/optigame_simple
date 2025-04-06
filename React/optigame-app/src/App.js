bb07195e823497a48c32031414c123e50140f49f
pinstripezebra <seelcs12@gmail.com>
2025-04-06 15:12:41 -0700

    adding react front end to api backend connection

--- React/optigame-app/src/App.js
+++ React/optigame-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [games, setGames] = useState([]);
+
 }
+
+const fetchGames = async () => {
+  const response = await api.get('/games');
+  setGames(response.data);
+}
+
+useEffect(() => {
+  fetchGames();
+}, []);
 
 export default App;
