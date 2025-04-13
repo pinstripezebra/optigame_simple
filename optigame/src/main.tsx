d4d9d01317c4ad61af5e8838f69b682108ab59ad
pinstripezebra <seelcs12@gmail.com>
2025-04-13 16:11:43 -0700

    adding chakra ui

--- optigame/src/main.tsx
+++ optigame/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "./components/ui/provider"
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 )
