436d96544999bd5e36aa62e9ccc51b90a7861d6b
pinstripezebra <seelcs12@gmail.com>
2025-04-29 18:09:02 -0700

    updating navbar

--- optigame/src/components/NavBar.tsx
+++ optigame/src/components/NavBar.tsx
@@ -1,21 +1,15 @@
-import { HStack, Image, Text, Spacer } from '@chakra-ui/react';
-import { Menu, MenuList, MenuButton, MenuItem, Button } from "@chakra-ui/react";
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import logo from '../assets/logo.jpg';
+import { HStack, Image, Text, Spacer, Menu, MenuList, MenuButton, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import logo from "../assets/logo.jpg";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      {/* Logo and Title */}
       <Image src={logo} boxSize="60px" />
       <Text fontSize="2xl" fontWeight="bold">
         Optigame
       </Text>
-
-      {/* Spacer to push the dropdown to the far right */}
       <Spacer />
-
-      {/* User Dropdown */}
       <Menu>
         <MenuButton as={Button}>
           User <ChevronDownIcon />
