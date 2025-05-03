4614bf8d5afc576ccf345b385be47c1bdafdab42
pinstripezebra <seelcs12@gmail.com>
2025-05-03 11:53:13 -0700

    not found page formatted

--- optigame/src/pages/NotFoundPage.tsx
+++ optigame/src/pages/NotFoundPage.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import {Text} from "@chakra-ui/react";
+import React from "react";
+import { Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
-    return (
-        <div>404 Page not found</div>
-    );
+  return (
+    <div>
+      <Text fontSize="xl" marginBottom="4">
+        404 Page not found
+      </Text>
+      <Button as={Link} to="/" colorScheme="teal" size="md">
+        Go back to Home
+      </Button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
