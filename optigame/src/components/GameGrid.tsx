071b43e92a4ad2a75f4e3f5772104ffc38dc4515
pinstripezebra <seelcs12@gmail.com>
2025-05-11 15:27:18 -0700

    adding loading skeleton to gamecard

--- optigame/src/components/GameGrid.tsx
+++ optigame/src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 export interface Game {
   id: string;
@@ -15,11 +16,19 @@ export interface Game {
 
 interface GameGridProps {
   games: Game[]; // Filtered games passed from the parent
+  error: string | null; // Error message
+  loading: boolean; // Loading state
 }
 
-const GameGrid = ({ games }: GameGridProps) => {
+const GameGrid = ({ games, error, loading }: GameGridProps) => {
+
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={10}>
+      {loading &&
+        skeletons.map((skeleton) => (
+          <GameCardSkeleton key={skeleton} />
+        ))}
       {games.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
