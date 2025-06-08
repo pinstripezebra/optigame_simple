a2bb8c1711b0efeb431de91e4231f7ff71fc4c57
pinstripezebra <seelcs12@gmail.com>
2025-06-07 17:09:02 -0700

    adding landing page outline

---
+++ optigame/src/pages/UserProfile/LandingPage/components/Features.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Heading, SimpleGrid, VStack, Image, Text } from "@chakra-ui/react";
+import features from "../data/data.json";
+
+const Features = () => (
+  <Box py={10} px={4}>
+    <Heading as="h2" size="xl" textAlign="center" mb={10}>
+      Features
+    </Heading>
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+      {features.map((feature, idx) => (
+        <VStack key={idx} spacing={4} align="center">
+          <Image
+            src={feature.image}
+            alt={feature.title}
+            boxSize="96px"
+            borderRadius="full"
+            objectFit="cover"
+            bg="gray.100"
+          />
+          <Text fontWeight="bold" fontSize="lg" textAlign="center">
+            {feature.title}
+          </Text>
+          <Text color="gray.600" textAlign="center">
+            {feature.text}
+          </Text>
+        </VStack>
+      ))}
+    </SimpleGrid>
+  </Box>
+);
+
+export default Features;
