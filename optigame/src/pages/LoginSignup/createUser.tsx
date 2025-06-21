d729f4a91154e4a270aa830c3672111866f1b483
pinstripezebra <seelcs12@gmail.com>
2025-06-21 09:08:27 -0700

    updating backend create user api call

--- optigame/src/pages/LoginSignup/createUser.tsx
+++ optigame/src/pages/LoginSignup/createUser.tsx
@@ -1,14 +1,17 @@
 import api from "../../services/api-client";
 
-export const createUser = async (email: string, username: string, password: string) => {
-  try {
-    const response = await api.post("/v1/user", {
-      email,
-      username,
-      password,
-    });
-    return response.data;
-  } catch (error: any) {
-    throw error.response?.data?.detail || "Registration failed";
-  }
+interface CreateUserPayload {
+  username: string;
+  password: string;
+  email: string;
+  role?: string; 
+}
+
+export const createUser = ({ username, password, email, role = "user" }: CreateUserPayload) => {
+  return api.post("/v1/user/", {
+    username,
+    password,
+    email,
+    role: role || "user"
+  });
 };
